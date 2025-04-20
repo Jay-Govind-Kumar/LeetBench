0cf25981f59ce7c4f66f93ad5b5a94d5f799074b
Jay-Govind <govind.iq@gmail.com>
2025-04-20 17:03:56 +0530

    feat(healthcheck): add route and controller for health monitoring Created a healthcheck route and corresponding controller to verify server status. Useful for uptime monitoring and health diagnostics.

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,5 +2,8 @@ import express from 'express';
 
 const app = express();
 
+import healthCheckRouter from "./routes/healthCheck.routes.js";
+
+app.use("api/v1/healthcheck", healthCheckRouter)
 
 export default app;
