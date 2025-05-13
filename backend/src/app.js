4a67bcdecdb999403decd148dc8f42ec2428c9c3
Jay-Govind <govind.iq@gmail.com>
2025-05-13 11:32:10 +0530

    feat(server): add healthcheck route
    Implemented a basic healthcheck endpoint to verify server uptime and readiness.

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,17 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
+
+// import routes
+import healthCheckRouter from "./routes/healthcheck.routes.js";
+
+// default route
+app.get("/", (req, res) => {
+  res.send("Hello guys! Welcome to the LeetBench 🔥");
+});
+
+// routes
+app.use("/api/v1/healthcheck", healthCheckRouter);
 
 export default app;
--- backend/src/index.js
+++ backend/src/index.js
@@ -8,5 +8,5 @@ dotenv.config({
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`🚀 Server is running on http://localhost:${PORT}`);
 });
